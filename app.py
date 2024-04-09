2a387bc1107b48b1038e6b99e2367037d4624c60
Mark-Edwards-II <edwardsdeveloping.now@gmail.com>
2024-04-09 10:21:36 -0400

    comments, print, global

--- app.py
+++ app.py
@@ -6,43 +6,43 @@ import time
 # Create a Flask app
 app = Flask(__name__, template_folder='templates')
 
+# Global variable to control the loop
+SHOULD_CONTINUE = False
 
 # Create a route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-should_continue = False
-
-def loop_function():
-    global should_continue
-    while should_continue:
-        print("Loop is running")
-        time.sleep(1)  # Simulate work by sleeping
-
-
-
+# Create a route to start the loop
 @app.route('/start')
 def start():
-    global should_continue
-    if not should_continue:
-        should_continue = True
+    global SHOULD_CONTINUE
+    if not SHOULD_CONTINUE:
+        SHOULD_CONTINUE = True
         thread = threading.Thread(target=loop_function)
         thread.start()
-        print("Loop has started!")
+        # print("Loop has started!")
         return "Loop has started!"
     else:
-        print("Loop is already running.")
+        # print("Loop is already running.")
         return "Loop is already running."
 
+# Create a route to stop the loop
 @app.route('/stop')
 def stop():
-    global should_continue
-    should_continue = False
-    print("Loop has stopped!")
+    global SHOULD_CONTINUE
+    SHOULD_CONTINUE = False
+    # print("Loop has stopped!")
     return "Loop has stopped!"
 
-
+# Function to run in the loop
+def loop_function():
+    global SHOULD_CONTINUE
+    while SHOULD_CONTINUE:
+        print("Loop is running")
+        time.sleep(1)  # Simulate work by sleeping
+    print("Loop has stopped")
 
 if __name__ == '__main__':
     app.run()
